14579040997832f6bbe14e72a86a50f4b401b1d7
dyatskovskiy <d.yatskovskiy@ukr.net>
2024-07-24 23:43:26 +0300

    Updated variable in .env

--- src/modules/password/password.service.ts
+++ src/modules/password/password.service.ts
@@ -4,7 +4,7 @@ import * as bcrypt from 'bcrypt';
 @Injectable()
 export class PasswordService {
   async hashPassword(password: string): Promise<string> {
-    const salt = await bcrypt.genSalt(+process.env.SALT_ROUNDS);
+    const salt = await bcrypt.genSalt(+process.env.BCRYPT_SALT_ROUNDS);
     console.log(salt);
     const hash = await bcrypt.hash(password, salt);
     console.log(hash);
